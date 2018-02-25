d8fb5ed4abb7a17080535189335e4b5586ca1a5b
Dmitry <d.gluhotorenko@gmail.com>
2018-02-25 23:22:45 +0300

    'fix'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 class Sorter {
   constructor() {
     this.Array = [];
+    this.COMPARATOR = (left, right) => left - right;
   return this;
   }
 
@@ -9,7 +10,7 @@ class Sorter {
   }
 
   at(index) {
-    // your implementation
+    return this.Array[index];
   }
 
   get length() {
@@ -21,24 +22,29 @@ class Sorter {
   }
 
   sort(indices) {
-    var i,j,min,temp;
-    for ( i=0; i< indices.length; i++){
+    var i, j, min, temp, g;
+    for ( i = 0; i < indices.length - 1; i++){
       min = i;
-        for (j=i+1; j< indices.length; j++){
-          if (this.Array[indices[j]]<this.Array[indices[min]]) {min=j};
+        for (j = i + 1; j < indices.length; j++){
+          if (indices[i] > indices[j]) {
+            g = indices[i];
+            indices[i] = indices[j];
+            indices[j] = g;
+          }
+          if (this.COMPARATOR(this.Array[indices[min]], this.Array[indices[j]]) > 0) { min = j };
         }
       temp = this.Array[indices[i]];
       this.Array[indices[i]] = this.Array[indices[min]] ;
       this.Array[indices[min]] = temp;
-
     }
     return this.Array;
   }
 
-  setComparator(compareFunction) {
-    return compareFunction (this.Array);
-  }
+    setComparator(compareFunction) {
+    this.COMPARATOR = compareFunction;
+    }
 
 }
+
 
 module.exports = Sorter;
